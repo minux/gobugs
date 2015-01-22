3f3c6a182f3f725fbd99afa9f8b3fe66eabb8cc3
Minux <minux@golang.org>
2015-01-21 20:03:11 -0500

    issue9546/main.c: also test log from FreeBSD msun.
    Signed-off-by: Minux <minux@golang.org>

--- issue9546_math.Log_accuracy/main.c
+++ issue9546_math.Log_accuracy/main.c
@@ -2,10 +2,12 @@
 #include <math.h>
 
 extern double __ieee754_log(double);
+extern double __fb_ieee754_log(double);
 int main() {
 #define PRINT(f) printf("%20s = %.20g\n", #f, (f))
 	double x = 0.6023203277884887;
 	printf("given x = %.20g:\n", x);
 	PRINT(__ieee754_log(x));
+	PRINT(__fb_ieee754_log(x));
 	PRINT(log(x));
 }

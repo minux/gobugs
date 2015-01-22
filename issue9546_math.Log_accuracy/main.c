976052a81ead6e8a9a9be3957e4410afe80b7f77
Minux <minux@golang.org>
2015-01-21 22:09:28 -0500

    issue9546/main.c: also test openlibm_log.
    The only two working log implementations so far are: openlibm and
    glibc (its implementation comes from "IBM Accurate Mathematical Library",
    and uses multiple precision floating point in the last step, see
    https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/ieee754/dbl-64/e_log.c;h=7a498e83)
    
    Signed-off-by: Minux <minux@golang.org>

--- issue9546_math.Log_accuracy/main.c
+++ issue9546_math.Log_accuracy/main.c
@@ -3,11 +3,13 @@
 
 extern double __ieee754_log(double);
 extern double __fb_ieee754_log(double);
+extern double openlibm_log(double);
 int main() {
 #define PRINT(f) printf("%20s = %.20g\n", #f, (f))
 	double x = 0.6023203277884887;
 	printf("given x = %.20g:\n", x);
 	PRINT(__ieee754_log(x));
 	PRINT(__fb_ieee754_log(x));
+	PRINT(openlibm_log(x));
 	PRINT(log(x));
 }

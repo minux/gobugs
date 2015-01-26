3ea64a7cafae7834cbe5bf1dd79c17e4e7ce9ab4
Minux <minux@golang.org>
2015-01-26 03:46:26 -0500

    isuse9546: add "bench" make target.
    Signed-off-by: Minux <minux@golang.org>

--- issue9546_math.Log_accuracy/main.c
+++ issue9546_math.Log_accuracy/main.c
@@ -5,11 +5,22 @@ extern double __ieee754_log(double);
 extern double __fb_ieee754_log(double);
 extern double openlibm_log(double);
 int main() {
-#define PRINT(f) printf("%20s = %.20g\n", #f, (f))
+#ifdef BENCH
+	volatile
+#endif
 	double x = 0.6023203277884887;
+#ifndef BENCH
+#define PRINT(f) printf("%20s = %.20g\n", #f, (f))
 	printf("given x = %.20g:\n", x);
 	PRINT(__ieee754_log(x));
 	PRINT(__fb_ieee754_log(x));
 	PRINT(openlibm_log(x));
 	PRINT(log(x));
+#else
+	int i;
+	volatile double y;
+	for (i = 0; i < TIMES; i++) {
+		y = LOG(x);
+	}
+#endif
 }
